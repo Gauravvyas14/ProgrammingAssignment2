8a540d888fe8fcf6ea0211dbb742406ddacc7f6f
Gauravvyas14 <vyas.gaurav14@gmail.com>
2015-04-23 11:02:03 +0530

    rprog-013-assignment-2-submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## functions to compute the inverse of matrix and return it from cache
 
-   ##---------makeCacheMatrix-------####
+   ##---------MAKE-CACHE-MATRIX--------####
 
 ## this function returns a list of functions to set and get the values of matrix and its Inverse
 ## function set -> if input of makecahematrix is changed it assigns new value to the old variable which then be computed for inverse
@@ -24,10 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
- ##---------------------cacheSolve----------------##
+ ##---------------------CACHE-SOLVE----------------##
 
 ## a function to check if inverse is in the cache or not, 
-## if its already computed it will set it in setinverse function else it will compute it 
+## if its already computed it will return it from cache else it will compute and set it in setinverse function
 ## when we run this function for the first time it computes the inverse of supplied matrix by function solve()
 ## all other times when ran it by same input value , it gets inverse from cache and returns it 
 
@@ -46,3 +46,29 @@ cacheSolve <- function(x, ...) {
 	x$setinverse(inv)
 	inv
 }
+
+
+##----------------------SAMPLE-OUTPUT------------------##
+##  x <- matrix((rnorm(16)), nrow= 4, ncol = 4)
+##  m <- makeCacheMatrix(x)
+##  m$get() 
+##   		[,1]       [,2]       [,3]       [,4]
+##	[1,]  0.8509464 -0.8969362  1.5400567  0.6881839
+##	[2,]  1.3769274 -0.9388900  0.1799797 -0.1291284
+##	[3,] -0.5838616  1.5117345 -0.6439339  0.4995189
+##	[4,] -0.7529193 -1.5797879 -0.4986466  1.5340340
+##  cacheSolve(m)
+##  Computing inverse Matrix...
+##          [,1]         [,2]       [,3]        [,4]
+##	[1,] 0.05599154  0.824421706  0.4413350 -0.09943158
+##	[2,] 0.13866189  0.005901135  0.5095497 -0.22763026
+##	[3,] 0.54403361 -0.555003915 -0.2430668 -0.21162859
+##	[4,] 0.34712008  0.230304383  0.6623488  0.29986338
+
+##   cacheSolve(m)
+##   getting inverse matrix from cache...
+##           [,1]         [,2]       [,3]        [,4]
+##	[1,] 0.05599154  0.824421706  0.4413350 -0.09943158
+##	[2,] 0.13866189  0.005901135  0.5095497 -0.22763026
+##	[3,] 0.54403361 -0.555003915 -0.2430668 -0.21162859
+##	[4,] 0.34712008  0.230304383  0.6623488  0.29986338
